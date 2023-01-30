ac0da7226a86d0677846b4a2afd0fbddd775524e
Wolf1938 <38575320+Wolf1938@users.noreply.github.com>
2023-01-30 20:44:14 +0000

    Branch Custom Inspector

--- Assets/Editor/Custom Inspector Scripts/ConversationEventDrawer.cs
+++ Assets/Editor/Custom Inspector Scripts/ConversationEventDrawer.cs
@@ -4,86 +4,218 @@ using UnityEditor;
 [CustomPropertyDrawer(typeof(ConversationEvent))]
 public class ConversationEventDrawer : PropertyDrawer
 {
-    private const int lineHeightOffset = 2;
-    private float lineHeight = EditorGUIUtility.singleLineHeight + lineHeightOffset;
+    private float lineHeight = EditorGUIUtility.singleLineHeight + 2;
     private int[] expandedHeight = { 6, 8, 2, 2 };
+
+    SerializedProperty EventType;
+    SerializedProperty UITemplate;
+    SerializedProperty Character;
+    SerializedProperty Text;
+    SerializedProperty BranchType;
+    SerializedProperty BranchEvents;
+    SerializedProperty BranchConversations;
+    SerializedProperty Quest;
+    SerializedProperty Cutscene;
+
+    private void Initialise(SerializedProperty property)
+    {
+        EventType = property.FindPropertyRelative("<EventType>k__BackingField");
+
+        UITemplate = property.FindPropertyRelative("<UITemplate>k__BackingField");
+        Character = property.FindPropertyRelative("<Character>k__BackingField");
+        Text = property.FindPropertyRelative("<Text>k__BackingField");
+
+        BranchType = property.FindPropertyRelative("<BranchType>k__BackingField");
+        BranchEvents = property.FindPropertyRelative("<BranchEvents>k__BackingField");
+        BranchConversations = property.FindPropertyRelative("<BranchConversations>k__BackingField");
+
+        Quest = property.FindPropertyRelative("<Quest>k__BackingField");
+        Cutscene = property.FindPropertyRelative("<Cutscene>k__BackingField");
+    }
 
     public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
     {
-        SerializedProperty EventType = property.FindPropertyRelative("<EventType>k__BackingField");
-        int totalLines = 1;
+        Initialise(property);
 
         if (property.isExpanded)
         {
-            totalLines += expandedHeight[EventType.intValue];
+            int totalLines = 1 + expandedHeight[EventType.intValue];
+
+            float height = GetHeight(totalLines);
+            if (EventType.intValue == (int)ConversationEvent.ConversationEventTypes.BRANCH)
+            {
+                if (BranchType.intValue == (int)ConversationEvent.BranchTypes.SHALLOW && BranchEvents.isExpanded)
+                {
+                    height += EditorGUI.GetPropertyHeight(BranchEvents);
+                }
+                else if (BranchType.intValue == (int)ConversationEvent.BranchTypes.DEEP && BranchConversations.isExpanded)
+                {
+                    height += EditorGUI.GetPropertyHeight(BranchConversations) + GetHeight(1);
+                }
+            }
+            return height;
         }
 
-        return lineHeight * totalLines + EditorGUIUtility.standardVerticalSpacing * totalLines;
+        return GetHeight(1);
+    }
+
+    private float GetHeight(int lines)
+    {
+        return lineHeight * lines + EditorGUIUtility.standardVerticalSpacing * lines;
     }
 
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
-        SerializedProperty EventType = property.FindPropertyRelative("<EventType>k__BackingField");
-        SerializedProperty UITemplate = property.FindPropertyRelative("<UITemplate>k__BackingField");
-        SerializedProperty Character = property.FindPropertyRelative("<Character>k__BackingField");
-        SerializedProperty Text = property.FindPropertyRelative("<Text>k__BackingField");
-        SerializedProperty Quest = property.FindPropertyRelative("<Quest>k__BackingField");
-        SerializedProperty Cutscene = property.FindPropertyRelative("<Cutscene>k__BackingField");
+        Initialise(property);
 
         EditorGUI.BeginProperty(position, label, property);
 
         Rect rectFoldout = new Rect(position.min.x, position.min.y, position.size.x, lineHeight);
-        property.isExpanded = EditorGUI.Foldout(rectFoldout, property.isExpanded, System.Enum.GetName(typeof(ConversationEvent.ConversationEventType), EventType.intValue));
+        property.isExpanded = EditorGUI.Foldout(rectFoldout, property.isExpanded, System.Enum.GetName(typeof(ConversationEvent.ConversationEventTypes), EventType.intValue));
         int lines = 1;
 
         if (property.isExpanded)
         {
-            Rect rectType = new Rect(position.min.x, position.min.y + lines++ * lineHeight, position.size.x, lineHeight);
-            EditorGUI.PropertyField(rectType, EventType);
+            EditorGUI.PropertyField(new Rect(position.min.x, position.min.y + lines++ * lineHeight, position.size.x, lineHeight), EventType);
 
             switch (EventType.intValue)
             {
-                case (int)ConversationEvent.ConversationEventType.SPEECH:
+                case (int)ConversationEvent.ConversationEventTypes.SPEECH:
+                    lines = DrawSpeech(position, lines);
+                    break;
+
+                case (int)ConversationEvent.ConversationEventTypes.BRANCH:
+                    lines = DrawSpeech(position, lines);
+                    EditorGUI.PropertyField(new Rect(position.min.x, position.min.y + lines++ * lineHeight, position.size.x, lineHeight), BranchType);
+
+                    switch (BranchType.intValue)
                     {
-                        Rect rectTemplate = new Rect(position.min.x, position.min.y + lines++ * lineHeight, position.size.x, lineHeight);
-                        Rect rectCharacter = new Rect(position.min.x, position.min.y + lines * lineHeight, position.size.x, lineHeight);
-                        Rect rectText = new Rect(position.min.x, position.min.y + lines++ * lineHeight + lineHeightOffset * 2, position.size.x, lineHeight * 4);
-                        
-                        EditorGUI.PropertyField(rectTemplate, UITemplate);
-                        EditorGUI.PropertyField(rectCharacter, Character);
-                        EditorGUI.PropertyField(rectText, Text, GUIContent.none);
+                        case (int)ConversationEvent.BranchTypes.SHALLOW:
+                            BranchEvents.arraySize = BranchEvents.arraySize < 2 ? 2 : BranchEvents.arraySize > 4 ? 4 : BranchEvents.arraySize;
+                            EditorGUI.PropertyField(new Rect(position.min.x, position.min.y + lines++ * lineHeight, position.size.x, lineHeight), BranchEvents);
+                            lines += (int)(EditorGUI.GetPropertyHeight(BranchEvents) / lineHeight);
+                            break;
+
+                        case (int)ConversationEvent.BranchTypes.DEEP:
+                            BranchConversations.arraySize = BranchConversations.arraySize < 2 ? 2 : BranchConversations.arraySize > 4 ? 4 : BranchConversations.arraySize;
+                            EditorGUI.PropertyField(new Rect(position.min.x, position.min.y + lines++ * lineHeight, position.size.x, lineHeight), BranchConversations);
+                            lines += (int)(EditorGUI.GetPropertyHeight(BranchConversations) / lineHeight);
+                            EditorGUI.HelpBox(new Rect(position.min.x, position.min.y + lines++ * lineHeight + 6, position.size.x, lineHeight), "Deep Branch is the last node, any other events will *not* be processed", MessageType.Info);
+                            break;
                     }
+
                     break;
 
-                case (int)ConversationEvent.ConversationEventType.BRANCH:
-                    {
-                        Rect rectTemplate = new Rect(position.min.x, position.min.y + lines++ * lineHeight, position.size.x, lineHeight);
-                        Rect rectCharacter = new Rect(position.min.x, position.min.y + lines * lineHeight, position.size.x, lineHeight);
-                        Rect rectText = new Rect(position.min.x, position.min.y + lines++ * lineHeight + lineHeightOffset * 2, position.size.x, lineHeight * 4);
-
-                        EditorGUI.PropertyField(rectTemplate, UITemplate);
-                        EditorGUI.PropertyField(rectCharacter, Character);
-                        EditorGUI.PropertyField(rectText, Text, GUIContent.none);
-                        // Branch code
-
-                        lines += 5;
-                        Rect rectHelpBox = new Rect(position.min.x, position.min.y + lines++ * lineHeight, position.size.x, lineHeight);
-                        EditorGUI.HelpBox(rectHelpBox, "Branch is the last node, any other events will *not* be processed", MessageType.Info);
-                    }
+                case (int)ConversationEvent.ConversationEventTypes.QUEST:
+                    EditorGUI.PropertyField(new Rect(position.min.x, position.min.y + lines++ * lineHeight, position.size.x, lineHeight), Quest);
                     break;
 
-                case (int)ConversationEvent.ConversationEventType.QUEST:
-                    Rect rectQuest = new Rect(position.min.x, position.min.y + lines++ * lineHeight, position.size.x, lineHeight);
-                    EditorGUI.PropertyField(rectQuest, Quest);
-                    break;
-
-                case (int)ConversationEvent.ConversationEventType.CUTSCENE:
-                    Rect rectCutscene = new Rect(position.min.x, position.min.y + lines++ * lineHeight, position.size.x, lineHeight);
-                    EditorGUI.PropertyField(rectCutscene, Cutscene);
+                case (int)ConversationEvent.ConversationEventTypes.CUTSCENE:
+                    EditorGUI.PropertyField(new Rect(position.min.x, position.min.y + lines++ * lineHeight, position.size.x, lineHeight), Cutscene);
                     break;
             }
         }
 
         EditorGUI.EndProperty();
+    }
+
+    // The property drawer for the speech event, returns the lines used
+    private int DrawSpeech(Rect position, int lines)
+    {
+        Rect rectTemplate = new Rect(position.min.x, position.min.y + lines++ * lineHeight, position.size.x, lineHeight);
+        Rect rectCharacter = new Rect(position.min.x, position.min.y + lines * lineHeight, position.size.x, lineHeight);
+        Rect rectText = new Rect(position.min.x, position.min.y + lines++ * lineHeight + 4, position.size.x, lineHeight * 4 + 4);
+
+        EditorGUI.PropertyField(rectTemplate, UITemplate);
+        EditorGUI.PropertyField(rectCharacter, Character);
+        EditorGUI.PropertyField(rectText, Text, GUIContent.none);
+
+        return lines + 4;
+    }
+}
+
+[CustomPropertyDrawer(typeof(ShallowBranchConversationEvent))]
+public class ShallowBranchConversationEventDrawer : PropertyDrawer
+{
+    private float lineHeight = EditorGUIUtility.singleLineHeight + 2;
+    private int[] expandedHeight = { 6, 2, 2 };
+
+    SerializedProperty EventType;
+
+    SerializedProperty UITemplate;
+    SerializedProperty Character;
+    SerializedProperty Text;
+
+    SerializedProperty Quest;
+
+    SerializedProperty Cutscene;
+
+    private void Initialise(SerializedProperty property)
+    {
+        EventType = property.FindPropertyRelative("<EventType>k__BackingField");
+
+        UITemplate = property.FindPropertyRelative("<UITemplate>k__BackingField");
+        Character = property.FindPropertyRelative("<Character>k__BackingField");
+        Text = property.FindPropertyRelative("<Text>k__BackingField");
+
+        Quest = property.FindPropertyRelative("<Quest>k__BackingField");
+        Cutscene = property.FindPropertyRelative("<Cutscene>k__BackingField");
+    }
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        Initialise(property);
+
+        int totalLines = property.isExpanded ? expandedHeight[EventType.intValue] + 1 : 1;
+        float height = lineHeight * totalLines + EditorGUIUtility.standardVerticalSpacing * totalLines;
+
+        return height;
+    }
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        Initialise(property);
+
+        EditorGUI.BeginProperty(position, label, property);
+
+        Rect rectFoldout = new Rect(position.min.x, position.min.y, position.size.x, lineHeight);
+        property.isExpanded = EditorGUI.Foldout(rectFoldout, property.isExpanded, System.Enum.GetName(typeof(ShallowBranchConversationEvent.ShallowBranchConversationEventTypes), EventType.intValue));
+        int lines = 1;
+
+        if (property.isExpanded)
+        {
+            EditorGUI.PropertyField(new Rect(position.min.x, position.min.y + lines++ * lineHeight, position.size.x, lineHeight), EventType);
+
+            switch (EventType.intValue)
+            {
+                case (int)ShallowBranchConversationEvent.ShallowBranchConversationEventTypes.SPEECH:
+                    lines = DrawSpeech(position, lines);
+                    break;
+
+                case (int)ShallowBranchConversationEvent.ShallowBranchConversationEventTypes.QUEST:
+                    EditorGUI.PropertyField(new Rect(position.min.x, position.min.y + lines++ * lineHeight, position.size.x, lineHeight), Quest);
+                    break;
+
+                case (int)ShallowBranchConversationEvent.ShallowBranchConversationEventTypes.CUTSCENE:
+                    EditorGUI.PropertyField(new Rect(position.min.x, position.min.y + lines++ * lineHeight, position.size.x, lineHeight), Cutscene);
+                    break;
+            }
+        }
+
+        EditorGUI.EndProperty();
+    }
+
+    // The property drawer for the speech event, returns the lines used
+    private int DrawSpeech(Rect position, int lines)
+    {
+        Rect rectTemplate = new Rect(position.min.x, position.min.y + lines++ * lineHeight, position.size.x, lineHeight);
+        Rect rectCharacter = new Rect(position.min.x, position.min.y + lines * lineHeight, position.size.x, lineHeight);
+        Rect rectText = new Rect(position.min.x, position.min.y + lines++ * lineHeight + 4, position.size.x, lineHeight * 4 + 4);
+
+        EditorGUI.PropertyField(rectTemplate, UITemplate);
+        EditorGUI.PropertyField(rectCharacter, Character);
+        EditorGUI.PropertyField(rectText, Text, GUIContent.none);
+
+        return lines + 4;
     }
 }
--- Assets/Scripts/Dialogue/Conversation.cs
+++ Assets/Scripts/Dialogue/Conversation.cs
@@ -15,10 +15,11 @@ public class Conversation : ScriptableObject
 [System.Serializable]
 public class ConversationEvent
 {
-    public enum ConversationEventType { SPEECH, BRANCH, QUEST, CUTSCENE }
+    public enum ConversationEventTypes { SPEECH, BRANCH, QUEST, CUTSCENE }
+    public enum BranchTypes { SHALLOW, DEEP,}
 
     #region Variables
-    [field: SerializeField] public ConversationEventType EventType { get; private set; } = ConversationEventType.SPEECH;
+    [field: SerializeField] public ConversationEventTypes EventType { get; private set; } = ConversationEventTypes.SPEECH;
 
     #region Speech
     [field: SerializeField] public ConversationUITemplate.ConversationUITemplates UITemplate { get; private set; }
@@ -29,7 +30,50 @@ public class ConversationEvent
     #endregion
 
     #region Branch
-    //[field: SerializeField] public ConversationEvent[] BranchEvents { get; private set; }
+    [field: SerializeField] public BranchTypes BranchType { get; private set; } = BranchTypes.SHALLOW;
+    [field: SerializeField] public BranchEvent[] BranchEvents { get; private set; } = new BranchEvent[0];
+    [field: SerializeField] public BranchConversation[] BranchConversations { get; private set; }
+    #endregion
+
+    #region Quest
+    [field: SerializeField] public GameObject Quest { get; private set; }
+    #endregion
+
+    #region Cutscene
+    [field: SerializeField] public GameObject Cutscene { get; private set; }
+    #endregion
+    #endregion
+}
+
+[System.Serializable]
+public class BranchEvent
+{
+    [field: SerializeField] public string ButtonText { get; private set; }
+    [field: SerializeField] public ShallowBranchConversationEvent[] Conversation { get; private set; } = new ShallowBranchConversationEvent[0];
+}
+
+[System.Serializable]
+public class BranchConversation
+{
+    [field: SerializeField] public string ButtonText { get; private set; }
+    
+    [field: SerializeField] public Conversation Conversation { get; private set; }
+}
+
+[System.Serializable]
+public class ShallowBranchConversationEvent
+{
+    public enum ShallowBranchConversationEventTypes { SPEECH, QUEST, CUTSCENE }
+
+    #region Variables
+    [field: SerializeField] public ShallowBranchConversationEventTypes EventType { get; private set; } = ShallowBranchConversationEventTypes.SPEECH;
+
+    #region Speech
+    [field: SerializeField] public ConversationUITemplate.ConversationUITemplates UITemplate { get; private set; }
+    [field: SerializeField] public Character Character { get; private set; }
+
+    [field: TextArea(3, 10)]
+    [field: SerializeField] public string Text { get; private set; }
     #endregion
 
     #region Quest
