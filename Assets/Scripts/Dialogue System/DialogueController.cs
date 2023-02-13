57f80f5477a68d3a9d90e9f7f27d53ca6530bd22
Maximilian Ansell <38575320+Wolf1938@users.noreply.github.com>
2023-02-13 23:37:57 +0000

    Merge Main into Dialogue Graph Branch (#14)
    * Polygon Collider added to tree branch Sprite Shape (#11)
    
    Just to prove that it works and the movement system doesn't freak out, the box collider for the ground has been removed, and the Closed Sprite Shape now has a Polygon Collider 2D that automatically updates to the size of the Sprite Shape.
    
    * Text effect wait links (#12)
    
    * Added Punctuation Delay Effects
    
    * Added change type speed Effect
    
    * Added Short and Long Waits
    
    Also Added variables to the texttype speeds
    
    ---------
    
    Co-authored-by: MAX ANSELL <1903439@uad.ac.uk>
    
    * Renamed some Script Folders
    
    ---------
    
    Co-authored-by: HPetch <60263607+HPetch@users.noreply.github.com>
    Co-authored-by: MAX ANSELL <1903439@uad.ac.uk>

--- Assets/Scripts/Dialogue System/DialogueController.cs
+++ Assets/Scripts/Dialogue System/DialogueController.cs
@@ -18,11 +18,10 @@ public class DialogueController : MonoBehaviour
     public event Action<Conversation> OnConversationEnd;
     #endregion
 
-    #region public Variables
+    #region Variables
     public bool IsConversing { get; private set; } = false;
-    #endregion
 
-    #region Private Variables
+    [field: SerializeField] private Conversation TestConversation { get; set; }
 
     private Queue<ConversationEvent> conversationEventQueue = new Queue<ConversationEvent>();
     private Queue<Conversation> conversationQueue = new Queue<Conversation>();
@@ -30,7 +29,7 @@ public class DialogueController : MonoBehaviour
     [SerializeField] private  ConversationUITemplate[] conversationUITemplates;
     private ConversationUITemplate uiTemplate;
 
-    private Conversation conversation = null;
+    private Conversation currentConversation = null;
     private ConversationEvent conversationEvent = null;
 
     // Cortoutines need to be referenced so they can be stopped prematurly in case of a skip
@@ -39,12 +38,39 @@ public class DialogueController : MonoBehaviour
 
     private string textTypeString = "";
 
+    [Tooltip("Delay between each char in the TextType Coroutine")]
+    [Range(0,0.25f)]
+    [SerializeField] public float TextTypeDelay = 0.01f;
+
+    [Tooltip("Delay when a period is used")]    
+    [Range(0,0.25f)]
+    [SerializeField] private float TextTypePeriodDelay = 0.05f;
+
+    [Tooltip("Delay when a comma is used")]
+    [Range(0, 0.25f)]
+    [SerializeField] private float TextTypeCommaDelay = 0.05f;
+
+    [Tooltip("Delay when a colon is used")]
+    [Range(0, 0.25f)]
+    [SerializeField] private float TextTypeColonDelay  = 0.05f;
+
+    [Tooltip("Delay when a semi-colon is used")]
+    [Range(0, 0.25f)]
+    [SerializeField] private float TextTypeSemiColonDelay = 0.05f;
     // Tracks wether the current char is rich text or not (TextTyper)
     private bool richText = false;
 
     private bool linkStarted = false;
-    // Delay between each char in the TextType Coroutine
-    private float textTypeDelay = 0.01f;
+
+    private float textTypeWaitTime = 0;
+    private float currentTextTypeDelay = 0.01f;
+
+    [Tooltip("Wait time when <link='wait_short'> is used")]
+    [Range(0, 0.5f)]
+    [SerializeField] private float shortWait = 0.2f;
+    [Tooltip("Wait time when <link='wait_long'> is used")]
+    [Range(0, 1f)]
+    [SerializeField] private float longWait = 0.5f;
     #endregion
 
     #region Functions
@@ -53,6 +79,11 @@ public class DialogueController : MonoBehaviour
     private void Awake()
     {
         Instance = this;
+    }
+
+    private void Start()
+    {
+        TriggerConversation(TestConversation);
     }
     #endregion
 
@@ -64,22 +95,22 @@ public class DialogueController : MonoBehaviour
         if (GameStateController.Instance.GameState != GameStateController.GameStates.DIALOGUE) return;
 
         // If the player inputed continue the conversation
-        if (Input.GetMouseButtonDown(0)) DisplayNext();        
+        if (Input.GetMouseButtonDown(0)) DisplayNext();
     }
 
     /// <summary>
     /// Starts a new conversation
     /// </summary>
-    public void TriggerConversation(Conversation _conversation)
+    public void TriggerConversation(Conversation conversation)
     {
         // If there is already a conversation ongoing add the triggered conversation to the queue and return
         if (IsConversing)
         {
-            conversationQueue.Enqueue(_conversation);
+            conversationQueue.Enqueue(conversation);
             return;
         }
 
-        conversation = _conversation;
+        currentConversation = conversation;
         // Queue each conversation event so they can be processed
         conversationEventQueue.Clear();
 
@@ -90,13 +121,13 @@ public class DialogueController : MonoBehaviour
     {
         IsConversing = true;
 
-        yield return new WaitForSeconds(conversation.ConversationStartDelay);
+        yield return new WaitForSeconds(currentConversation.ConversationStartDelay);
 
         // Limit player inputs while a dialogue sequence is playing
         GameStateController.Instance.DialoguePause();
 
-        foreach (ConversationEvent convoEvent in conversation.ConversationEvents) conversationEventQueue.Enqueue(convoEvent);
-        OnConversationStart?.Invoke(conversation);
+        foreach (ConversationEvent convoEvent in currentConversation.ConversationEvents) conversationEventQueue.Enqueue(convoEvent);
+        OnConversationStart?.Invoke(currentConversation);
 
         changeCharacter = StartCoroutine(ChangeCharacter(false));
     }
@@ -140,16 +171,20 @@ public class DialogueController : MonoBehaviour
     /// <summary>
     /// Text-Type coroutine
     /// </summary>
-    private IEnumerator TypeSentance(string _sentence)
+    private IEnumerator TypeSentance(string sentence)
     {
         // Set text field to blank      
         textTypeString = "";
         uiTemplate.TextField.GetComponent<TextEffect>().ClearText();
 
+        // Reset TextType Delay to the default delay (incase it was changed in a link)
+        currentTextTypeDelay = TextTypeDelay;
+
+
         // For each character
-        for (int letterIndex = 0; letterIndex < _sentence.Length; letterIndex++)
+        for (int letterIndex = 0; letterIndex < sentence.Length; letterIndex++)
         {
-            char letter = _sentence[letterIndex];
+            char letter = sentence[letterIndex];
 
             // Add that character to the string
             textTypeString += letter;
@@ -158,11 +193,16 @@ public class DialogueController : MonoBehaviour
             if (letter == '<')
             {
                 richText = true;
-                if(_sentence.Substring(letterIndex+1,4) == "link")
+                if (sentence.Substring(letterIndex + 1, 4) == "link")
                 {
                     linkStarted = true;
+
+                    // If the Short Wait Link is used in Rich text, Set the Wait time to short.
+                    if (sentence.Substring(letterIndex + 7, 10) == "wait_short") textTypeWaitTime = shortWait;
+                    // If the long Wait Link is used in Rich text, Set the Wait time to long.
+                    if (sentence.Substring(letterIndex + 7, 9) == "wait_long") textTypeWaitTime = longWait;
                 }
-                else if (_sentence.Substring(letterIndex + 1, 5) == "/link")
+                else if (sentence.Substring(letterIndex + 1, 5) == "/link")
                 {
                     linkStarted = false;
                 }
@@ -184,20 +224,60 @@ public class DialogueController : MonoBehaviour
                 uiTemplate.TextField.text += "</link>";
             }
 
-            if (letter == ' ')
+            // Update the text.
+            uiTemplate.TextField.GetComponent<TextEffect>().UpdateText();
+            
+            // If there is a TextTypeWaitTime set, then wait.
+            if (textTypeWaitTime > 0f)
             {
-                continue;
+                yield return new WaitForSeconds(textTypeWaitTime);
+                // Once wait has been completed set variable to 0.
+                textTypeWaitTime = 0f;
             }
 
-            uiTemplate.TextField.GetComponent<TextEffect>().UpdateText();
-            yield return new WaitForSeconds(textTypeDelay);
+            switch (letter)
+            {
+                // If the char is a space
+                case ' ':
+                    // skip to next letter
+                    continue;
+
+                    // If the char is a period
+                case '.':
+                    // Wait for the duration of TextTypePeriodDelay
+                    yield return new WaitForSeconds(TextTypePeriodDelay);
+                    continue;
+
+                // If the char is a Comma
+                case ',':
+                    // Wait for the duration of TextTypeCommaDelay
+                    yield return new WaitForSeconds(TextTypeCommaDelay);
+                    continue;
+
+                // If the char is a Colon
+                case ':':
+                    // Wait for the duration of TextTypeColonDelay
+                    yield return new WaitForSeconds(TextTypeColonDelay);
+                    continue;
+
+                // If the char is a Semi-Colon
+                case ';':
+                    // Wait for the duration of TextTypeSemiColonDelay
+                    yield return new WaitForSeconds(TextTypeSemiColonDelay);
+                    continue;
+
+                    // Else for every other character use the default delay
+                default:
+                    yield return new WaitForSeconds(currentTextTypeDelay);
+                    continue;
+            }
         }
 
         // Sets coroutine to null, to track when it's finished
         textType = null;
     }
 
-    private IEnumerator ChangeCharacter(bool _isOpen)
+    private IEnumerator ChangeCharacter(bool isOpen)
     {
         /*if (_isOpen)
         {
@@ -227,7 +307,7 @@ public class DialogueController : MonoBehaviour
     {
         //animator.SetTrigger("Closed");
 
-        yield return new WaitForSeconds(conversation.ConversationEndDelay);
+        yield return new WaitForSeconds(currentConversation.ConversationEndDelay);
 
         //if (triggeredObjective) ObjectiveController.Instance.AddObjective(triggeredObjective);
 
@@ -238,8 +318,8 @@ public class DialogueController : MonoBehaviour
         textType = null;
         changeCharacter = null;
 
-        OnConversationEnd?.Invoke(conversation);
-        conversation = null;
+        OnConversationEnd?.Invoke(currentConversation);
+        currentConversation = null;
 
         if (conversationQueue.Count > 0)
         {
@@ -247,9 +327,14 @@ public class DialogueController : MonoBehaviour
         }
     }
 
-    public void BranchButton(int _buttonID)
+    public void BranchButton(int buttonID)
     {
 
-    }    
+    }
+
+    public void SetTypeSpeed(float textTypeDelay)
+    {
+        currentTextTypeDelay = textTypeDelay;
+    }
     #endregion
 }
