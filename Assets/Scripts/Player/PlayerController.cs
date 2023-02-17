4514bb89f119fb6a73f4fe2597795e01f5dd8f77
Wolf1938 <38575320+Wolf1938@users.noreply.github.com>
2023-02-17 00:33:31 +0000

    Wall Player Mechanics
    Added Wall Jump
    Added Wall Hop
    Added Wall Slide
    Added Wall Stuck
    Added Variable Jump Height
    Added Air Movement
    Added Player Effect script

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -17,8 +17,12 @@ public class PlayerController : MonoBehaviour
     public event Action OnPlayerJump;
     public event Action OnPlayerGroundJump;
     public event Action OnPlayerAirJump;
+    public event Action OnPlayerWallHop;
+    public event Action OnPlayerWallJump;
+    public event Action OnPlayerWallHit;
     public event Action OnPlayerLand;
     public event Action OnPlayerSlide;
+    public event Action OnPlayerDash;
     #endregion
 
     #region Variables
@@ -26,18 +30,32 @@ public class PlayerController : MonoBehaviour
     public bool IsGrounded { get; private set; } = true;
     public bool IsSliding { get; private set; } = false;
     public bool IsDashing { get; private set; } = false;
+    public bool IsTouchingWall { get; private set; } = false;
+    public bool IsWallStuck { get; private set; } = false;
+    public bool IsWallSliding { get; private set; } = false;
+    public bool IsGliding { get; private set; } = false;
     public bool IsLockedInput { get; private set; } = false;
+    public bool IsFacingRight { get; set; } = true;
 
     // Component References
     private Rigidbody2D rb;
-    private Animator animator;
     private CapsuleCollider2D capsuleCollider;
-    private SpriteRenderer spriteRenderer;
 
     [Header("Movement Settings")]
+
+    [Range(5, 15)]
+    [Tooltip("How fast the player moves on the ground")]
     [SerializeField] private float moveSpeed = 10f;
+
+    [Range(0, 80)]
+    [Tooltip("How fast the player accelerates in the air")]
+    [SerializeField] private float airMoveAcceleration = 50f;
+
+    [Range(0, 2)]
+    [Tooltip("How fast the player decelerates when in air and no input")]
+    [SerializeField] private float airDragMultiplier = 0.95f;
+
     private Vector2 movementInput = Vector2.zero;
-    private bool isFacingRight = true;
 
     #region Jump
     [Space(10)]
@@ -53,6 +71,10 @@ public class PlayerController : MonoBehaviour
     [Tooltip("The jump force when the Player jumps while in the air (Not grounded)")]
     [Range(0, 30)]
     [SerializeField] private float airJumpForce = 14f;
+
+    [Range(0, 1)]
+    [Tooltip("Variable jump height strenth")]
+    [SerializeField] private float variableJumpHegithMultiplier = 0.5f;
 
     [Tooltip("Stores how many times can the player air jump")]
     [Range(0, 3)]
@@ -70,6 +92,45 @@ public class PlayerController : MonoBehaviour
     private float timeOfLastGroundedJump = 0f;
     // Tracks if the jump action input has been queued
     private bool jumpQueued = false;
+    #endregion
+
+    #region Wall
+    [Space(10)]
+    [Header("Wall Settings")]
+
+    [Tooltip("Stores which layers should be used in the IsTouchingWall check")]
+    [SerializeField] private LayerMask wallLayerMask;
+
+    [Tooltip("The speed that the player slides down the wall")]
+    [Range(0, 5)]
+    [SerializeField] private float wallSlideSpeed = 2f;
+
+    [Tooltip("The jump force when the Player jumps from the wall")]
+    [Range(0, 2)]
+    [SerializeField] private float wallCheckDistance = 1f;
+
+    [Space(5)]
+    [Tooltip("The jump force when the Player hops from the wall")]
+    [Range(0, 30)]
+    [SerializeField] private float wallHopForce = 10f;
+
+    [Tooltip("The direction of the wall hop, Normalized during Initialisation")]
+    [SerializeField] private Vector2 wallHopDirection = new Vector2(1, 0.5f);
+
+    [Space(5)]
+    [Tooltip("The jump force when the Player jumps from the wall")]
+    [Range(0, 30)]
+    [SerializeField] private float wallJumpForce = 20f;
+
+    [Tooltip("The direction of the wall jump, Normalized during Initialisation")]
+    [SerializeField] private Vector2 wallJumpDirection = new Vector2(1, 2);
+
+    [Tooltip("The delay in seconds between the Player hitting the wall and beggining a wall slide")]
+    [Range(0, 1)]
+    [SerializeField] private float wallStickTime = 0.3f;
+
+    private float timeOfLastWallHit = 0f;
+
     #endregion
 
     #region Slide
@@ -100,8 +161,8 @@ public class PlayerController : MonoBehaviour
     [Space(10)]
     [Header("Dash Settings")]
     [Tooltip("The speed of the dash")]
-    [Range(0, 20)]
-    [SerializeField] private float dashSpeed = 1f;
+    [Range(0, 30)]
+    [SerializeField] private float dashSpeed = 20f;
 
     [Tooltip("The duration the IsDashing state is true")]
     [Range(0, 2)]
@@ -113,7 +174,6 @@ public class PlayerController : MonoBehaviour
 
     private float timeOfLastDash = 0f;
     private bool dashQueued = false;
-    private float TimeSinceLastDash { get { return Time.time - timeOfLastDash; } }
     #endregion
 
     #endregion
@@ -130,8 +190,8 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         capsuleCollider = GetComponent<CapsuleCollider2D>();
 
-        animator = GetComponentInChildren<Animator>();
-        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        wallHopDirection.Normalize();
+        wallJumpDirection.Normalize();
     }
 
     private void Start()
@@ -146,29 +206,75 @@ public class PlayerController : MonoBehaviour
     {
         movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         CheckMovementDirection();
-        IsGrounded = CheckIfPlayerIsGrounded();
+
+        CheckIfPlayerIsGrounded();
+        CheckIfWallTouching();
+        CheckIfWallStuck();
+        CheckIfWallSliding();
+        CheckIfDashing();
 
         HandleActionInput();
-        UpdateAnimationController();
     }
 
     private void FixedUpdate()
     {
-        SlidingBehaviour();
+        if (IsSliding) SlidingBehaviour();
 
         if (CanMove) ApplyMovement();
-
     }
     #endregion
 
     private void CheckMovementDirection()
     {
-        if ((isFacingRight && movementInput.x < 0) || (!isFacingRight && movementInput.x > 0)) isFacingRight = !isFacingRight;
+        if (!IsWallSliding && !IsWallStuck)
+        {
+            if ((IsFacingRight && movementInput.x < 0) || (!IsFacingRight && movementInput.x > 0))
+            {
+                Flip();
+            }
+        }
     }
 
     private void ApplyMovement()
     {
-        rb.velocity = new Vector2(movementInput.x * moveSpeed, rb.velocity.y);
+        Vector2 velocity = rb.velocity;
+
+        if (IsGrounded)
+        {
+            velocity = new Vector2(movementInput.x * moveSpeed, velocity.y);
+        }
+        // If NotGrounded and Not touching a wall
+        else if (!IsTouchingWall)
+        {
+            // If Movement Input
+            if (movementInput.x != 0)
+            {
+                // Add the air acceleration force
+                velocity.x += movementInput.x * airMoveAcceleration;
+
+                // If we exceed the player move speed
+                if (MathF.Abs(velocity.x) > moveSpeed)
+                {
+                    // Clamp the movespeed
+                    velocity.x = moveSpeed * movementInput.x;
+                }
+            }
+            // If no Movement Input
+            else
+            {
+                // Slow the player down by the air drag
+                velocity.x *= airDragMultiplier;
+            }
+        }
+
+        // If wallSliding and the Player is falling faster than the wall slide speed, then set the Y velocity to wall slide speed
+        if (IsWallSliding && velocity.y < -wallSlideSpeed)
+        {
+            velocity.y = -wallSlideSpeed;
+        }
+
+        // Finally set the velocity
+        rb.velocity = velocity;
     }
 
     #region ActionInputs
@@ -177,8 +283,6 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     private void HandleActionInput()
     {
-        if (IsDashing && TimeSinceLastDash > dashDuration) IsDashing = false;
-
         // If the dialogue or transition systems have locked the player's input
         if (IsLockedInput) return;
 
@@ -188,6 +292,10 @@ public class PlayerController : MonoBehaviour
         {
             if (CanJump) Jump();
             else jumpQueued = true;
+        } 
+        else if (Input.GetButtonUp("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * variableJumpHegithMultiplier);
         }
 
         if (Input.GetButtonDown("Slide"))
@@ -236,6 +344,8 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     private void Jump()
     {
+        UnWallSick();
+
         // If player is grounded do a ground jump
         if (IsGrounded)
         {
@@ -243,6 +353,22 @@ public class PlayerController : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, groundedJumpForce);
             timeOfLastGroundedJump = Time.time;
             OnPlayerGroundJump?.Invoke();
+        }
+        // If the player is not grounded, is wall sliding and has no input do a Wall Hop
+        else if (IsTouchingWall && movementInput.x == 0)
+        {
+            Flip();
+            rb.AddForce(new Vector2(FacingDirection * wallHopForce * wallHopDirection.x, wallHopForce * wallHopDirection.y), ForceMode2D.Impulse);
+
+            OnPlayerWallHop?.Invoke();
+        }
+        // If the player is not grounded, is touching the wall, and there is input do a Wall Jump
+        // IsTouchingWall used instead of IsWallSliding incase the Y velocity is positive
+        else if (IsTouchingWall && movementInput.x != 0)
+        {
+            rb.AddForce(new Vector2(-FacingDirection * wallJumpForce * wallJumpDirection.x, wallJumpForce * wallJumpDirection.y), ForceMode2D.Impulse);
+
+            OnPlayerWallJump?.Invoke();
         }
         // Else it's an air jump
         else
@@ -252,8 +378,6 @@ public class PlayerController : MonoBehaviour
             OnPlayerAirJump?.Invoke();
         }
 
-        //animator.Play("Player_Jump");
-        //source.PlayOneShot(jumpClip);
         OnPlayerJump?.Invoke();
     }
 
@@ -262,13 +386,9 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     private void Slide()
     {
-        //source.PlayOneShot(slideClip);
-
         IsSliding = true;
         slideTimeElapsed = 0;
         slideCurveSamplePoint = 0;
-
-        //animator.Play("Player_Slide");
 
         OnPlayerSlide?.Invoke();
     }
@@ -278,15 +398,12 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     private void SlidingBehaviour()
     {
-        if (IsSliding)
-        {
-            // Takes the facing direction, multiples that by the current point on the curve, and then multiplied by the slide
-            rb.velocity = new Vector2(FacingDirection * (slideCurve.Evaluate(slideCurveSamplePoint) * slideForce), rb.velocity.y);
+        // Takes the facing direction, multiples that by the current point on the curve, and then multiplied by the slide
+        rb.velocity = new Vector2(FacingDirection * (slideCurve.Evaluate(slideCurveSamplePoint) * slideForce), rb.velocity.y);
 
-            slideCurveSamplePoint += Time.fixedDeltaTime / slideDuration;   // As fixedDeltaTime is usually ~< 0.01, dividing it by our duration offsets it to reach 1 by the end of the slide
-            slideTimeElapsed += Time.fixedDeltaTime;
-            IsSliding = slideTimeElapsed <= slideDuration;
-        }
+        slideCurveSamplePoint += Time.fixedDeltaTime / slideDuration;   // As fixedDeltaTime is usually ~< 0.01, dividing it by our duration offsets it to reach 1 by the end of the slide
+        slideTimeElapsed += Time.fixedDeltaTime;
+        IsSliding = slideTimeElapsed <= slideDuration;
     }
 
     private void Dash()
@@ -294,20 +411,8 @@ public class PlayerController : MonoBehaviour
         IsDashing = true;
         timeOfLastDash = Time.time;
 
-        Debug.Log("Dash");
         rb.velocity = new Vector2(FacingDirection * dashSpeed, rb.velocity.y);
-    }
-    #endregion
-
-    #region Visuals
-    /// <summary>
-    /// Passes the state booleans.
-    /// </summary>
-    void UpdateAnimationController()
-    {
-        spriteRenderer.flipX = FacingDirection == 1 ? false : true;
-        animator.SetBool("Running", Mathf.Abs(movementInput.x) > 0.2f);
-        animator.SetBool("Grounded", IsGrounded);
+        OnPlayerDash?.Invoke();
     }
     #endregion
 
@@ -333,16 +438,16 @@ public class PlayerController : MonoBehaviour
     /// Uses a BoxCast on the platform layer to determine if the player is grounded, takes into consideration coyote time.
     /// </summary>
     /// <returns> Returns true if the player is either grounded or coyote grounded.</returns>
-    private bool CheckIfPlayerIsGrounded()
+    private void CheckIfPlayerIsGrounded()
     {
-        bool isGroundedThisFrame = Physics2D.BoxCast(capsuleCollider.bounds.center, capsuleCollider.bounds.size, 0f, Vector2.down, 0.001f, platformLayerMask);
+        bool isGroundedThisFrame = Physics2D.BoxCast(capsuleCollider.bounds.center, new Vector3(capsuleCollider.bounds.size.x * 0.75f, capsuleCollider.bounds.size.y, capsuleCollider.bounds.size.z), 0f, Vector2.down, 0.001f, platformLayerMask);
         DrawDebugInfo();
 
         // If the player is grounded this frame
         if (isGroundedThisFrame)
         {
             // And coyote time has passed since the last ground jump
-            if (Time.time - timeOfLastGroundedJump > coyoteTime)
+            if (TimeSinceLastGroundedJump > coyoteTime)
             {
                 // This is a true grounded
                 timeOfLastTrueIsGrounded = Time.time;
@@ -364,18 +469,77 @@ public class PlayerController : MonoBehaviour
             }
         }
         // If the player is not grounded this frame but is within coyote time and they didn't ground Jump, then they are grounded
-        else if (Time.time - timeOfLastTrueIsGrounded < coyoteTime && Time.time - timeOfLastGroundedJump > coyoteTime)
+        else if (TimeSinceLastTrueGround < coyoteTime && TimeSinceLastGroundedJump > coyoteTime)
         {
             isGroundedThisFrame = true;
         }
 
-        return isGroundedThisFrame;
+        IsGrounded = isGroundedThisFrame;
+    }
+
+    private void CheckIfWallTouching()
+    {
+        bool isTouchingWallThisFrame = Physics2D.Raycast(capsuleCollider.bounds.center, Vector2.right, wallCheckDistance, wallLayerMask) ||
+                                       Physics2D.Raycast(capsuleCollider.bounds.center, Vector2.left, wallCheckDistance, wallLayerMask);
+
+        // If player is not on the ground and is touching wall this frame, but not the last frame, then the player has hit a wall
+        if (!IsGrounded && isTouchingWallThisFrame && !IsTouchingWall)
+        {
+            timeOfLastWallHit = Time.time;
+            WallStick();
+
+            OnPlayerWallHit?.Invoke();
+        }
+
+        IsTouchingWall = isTouchingWallThisFrame;
+    }
+
+    private void CheckIfWallStuck()
+    {
+        // If IsWallStuck & wallStickTime is exceeded, then Unstick
+        if (IsWallStuck && TimeSinceLastWallHit > wallStickTime)
+        {
+            UnWallSick();
+        }
+    }
+
+    private void CheckIfWallSliding()
+    {
+        // Player IsWallSliding if they are not grounded, touching a wall, not wall stuck, and they are falling
+        IsWallSliding = !IsGrounded && IsTouchingWall && !IsWallStuck && rb.velocity.y <= 0;
+    }
+
+    private void CheckIfDashing()
+    {
+        // If Player IsDashing & within dash duration they are still dashing, else they are not
+        IsDashing = IsDashing && TimeSinceLastDash < dashDuration;
+    }
+
+    private void Flip()
+    {
+        IsFacingRight = !IsFacingRight;
+    }
+
+    private void WallStick()
+    {
+        IsWallStuck = true;
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionY;
+        
+        // Reset air jumps
+        remainingAirJumps = maxAirJumps;
+    }
+
+    private void UnWallSick()
+    {
+        IsWallStuck = false;
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        rb.velocity = new Vector2(0, -.1f);
     }
 
     /// <summary>
     /// Returns true if the Player is able to Jump.
     /// </summary>
-    private bool CanJump { get { return IsGrounded || remainingAirJumps > 0; } }
+    private bool CanJump { get { return IsGrounded || IsWallSliding || remainingAirJumps > 0; } }
 
     /// <summary>
     /// Returns true if the Player is able to Slide.
@@ -392,7 +556,12 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     private bool CanMove { get { return !IsSliding && !IsDashing; } }
 
-    private int FacingDirection { get { return isFacingRight? 1 : -1; } }
+    private int FacingDirection { get { return IsFacingRight? 1 : -1; } }
+
+    private float TimeSinceLastDash { get { return Time.time - timeOfLastDash; } }
+    private float TimeSinceLastTrueGround { get { return Time.time - timeOfLastTrueIsGrounded; } }
+    private float TimeSinceLastGroundedJump { get { return Time.time - timeOfLastGroundedJump; } }
+    private float TimeSinceLastWallHit { get { return Time.time - timeOfLastWallHit; } }
     #endregion
 
     #region Collision
---
+++ Assets/Scripts/Player/PlayerEffectController.cs
@@ -0,0 +1,129 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEffectController : MonoBehaviour
+{
+    private PlayerController player = null;
+
+    private Rigidbody2D rb;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+    [Header("Ground Jump Effects")]
+    [SerializeField] private GameObject groundJumpParticleEffect;
+    [SerializeField] private AudioClip groundJumpAudioClip;
+
+    [Header("Air Jump Effects")]
+    [SerializeField] private GameObject airJumpParticleEffect;
+    [SerializeField] private AudioClip airJumpAudioClip;
+
+    [Header("Wall Jump Effects")]
+    [SerializeField] private GameObject wallJumpParticleEffect;
+    [SerializeField] private AudioClip wallJumpAudioClip;
+
+    [Header("Wall Hop Effects")]
+    [SerializeField] private GameObject wallHopParticleEffect;
+    [SerializeField] private AudioClip wallHopAudioClip;
+
+    [Header("Wall Hit Effects")]
+    [SerializeField] private GameObject wallHitParticleEffect;
+    [SerializeField] private AudioClip wallHitAudioClip;
+
+    [Header("Wall Slide Effects")]
+    [SerializeField] private GameObject wallSlideParticleEffect;
+    [SerializeField] private AudioClip wallSlideAudioClip;
+
+    [Header("Land Effects")]
+    [SerializeField] private GameObject landParticleEffect;
+    [SerializeField] private AudioClip landAudioClip;
+
+    [Header("Slide Effects")]
+    [SerializeField] private GameObject slideParticleEffect;
+    [SerializeField] private AudioClip slideAudioClip;
+
+    [Header("Dash Effects")]
+    [SerializeField] private GameObject dashParticleEffect;
+    [SerializeField] private AudioClip dashAudioClip;
+
+    private void Awake()
+    {
+        player = GetComponentInParent<PlayerController>();
+
+        rb = GetComponentInParent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        player.OnPlayerJump += PlayerJump;
+        player.OnPlayerGroundJump += PlayerGroundJump;
+        player.OnPlayerAirJump += PlayerAirJump;
+        player.OnPlayerWallHop += PlayerWallHop;
+        player.OnPlayerWallJump += PlayerWallJump;
+        player.OnPlayerWallHit += PlayerWallHit;
+        player.OnPlayerLand += PlayerLanded;
+        player.OnPlayerSlide += PlayerSlide;
+        player.OnPlayerDash += PlayerDash;
+    }
+
+    private void Update()
+    {
+        UpdateAnimator();
+    }
+
+    private void UpdateAnimator()
+    {
+        if(!player.IsWallSliding || !player.IsWallStuck) spriteRenderer.flipX = !player.IsFacingRight;
+
+        animator.SetBool("Running", Mathf.Abs(rb.velocity.x) > 0.2f);
+        animator.SetBool("Grounded", player.IsGrounded);
+        animator.SetBool("Sliding", player.IsSliding);
+        animator.SetFloat("Y_Velocity", rb.velocity.y);
+    }
+
+    private void PlayerJump()
+    {
+
+    }
+
+    private void PlayerGroundJump()
+    {
+
+    }
+
+    private void PlayerAirJump()
+    {
+
+    }
+
+    private void PlayerWallHop()
+    {
+
+    }
+
+    private void PlayerWallJump()
+    {
+
+    }
+
+    private void PlayerWallHit()
+    {
+
+    }
+
+    private void PlayerLanded()
+    {
+
+    }
+
+
+    private void PlayerSlide()
+    {
+
+    }
+
+
+    private void PlayerDash()
+    {
+
+    }
+}
