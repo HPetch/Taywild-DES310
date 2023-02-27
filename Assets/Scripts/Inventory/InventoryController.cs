3600cad669d7d5cb52afabe5da3fd1dc714a1b93
MAX ANSELL <1903439@uad.ac.uk>
2023-02-27 14:59:33 +0000

    Update InventoryController.cs

--- Assets/Scripts/Inventory/InventoryController.cs
+++ Assets/Scripts/Inventory/InventoryController.cs
@@ -1,77 +1,102 @@
+// Inventory Controller
+// Uses a dictionary to track each item the player has stored.
+// Items can be added and removed with subsequent relevent events being fired
+// The OnItemQuantityChanged event is fired on both operations
+
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using Unity.Mathematics;
 using UnityEngine;
 
 public class InventoryController : MonoBehaviour
 {
+    public enum ItemNames { FLOWER, TWIG, ROOT }
+
     public static InventoryController Instance { get; private set; }
 
     #region Events
-    public event Action<ItemNames, int> OnAddItem; // Called when an item is added, returns the new total
-    public event Action<ItemNames, int> OnRemoveItem; // Called when an item is attempted be be subtracted, returns new value and weither the subtraction worked or not
-    public event Action<ItemNames, int> OnAmountChangeItem; // Called when item amount changes, ignores if item was added or subtracted, returns the new total
+    public event Action<ItemNames, int> OnItemAdded; 
+    public event Action<ItemNames, int> OnItemRemoved; 
+    public event Action<ItemNames, int> OnItemQuantityChanged;
     #endregion
 
-    #region Public variables
-    public enum ItemNames // Should be set to { get; private set; }
-    {
-        FLOWER, TWIG, ROOT
-    }
-
+    #region Variables
+    // The dictionary that represents the inventory
     private Dictionary<ItemNames, int> inventory = new Dictionary<ItemNames, int>();
-
     #endregion
 
     #region Functions
-
     #region Initialisation
     private void Awake()
     {
-        Instance = this;
+        // If there already exists an Instance of this singleton then destroy this object, else this is the singleton instance
+        if (Instance != null) Destroy(gameObject);
+        else Instance = this;
     }
     #endregion
 
-    public int CheckItem(ItemNames _item)
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="_item"> The item that you wish to enquire about</param>
+    /// <returns> The quantity of the specified item in the inventory dictionary</returns>
+    public int ItemQuantity(ItemNames _item)
     {
         if (InventoryDoesNotContainItem(_item)) return 0;
         else return inventory[_item];
     }
 
-    public void AddItem(ItemNames _item, int _amount)
+    /// <summary>
+    /// Adds a quantity of a specified item to the inventory
+    /// </summary>
+    /// <param name="_item"> The item you wish to add</param>
+    /// <param name="_quantity"> The quantity you wish to add</param>
+    public void AddItem(ItemNames _item, int _quantity)
     {
-        if (_amount > 0)
-        {
-            if (InventoryDoesNotContainItem(_item))
-            {
-                inventory.Add(_item, _amount);
-            }
-            else
-            {
-                Mathf.Clamp(inventory[_item] += _amount, 0, 100);
-            }
-            OnAmountChangeItem?.Invoke(_item, inventory[_item]);
-            OnAddItem?.Invoke(_item, inventory[_item]);
-        }
-        else Debug.LogWarning("Attempted to add item, but amount wasn't provided or was negative. Amount: " + _amount);
-        if (inventory[_item] >= 99) Debug.LogWarning("Maximum item value reached. Somthing must have gone wrong. Item: " + _item);
+        if (_quantity == 0) Debug.LogWarning("A quantity of 0 was added");
+
+        // Just in case a - quantity has been inputed
+        _quantity = Mathf.Abs(_quantity);
+
+        // If the item is already in the dictionary, then increment the quantity
+        if (InventoryContainsItem(_item)) inventory[_item] += _quantity;
+        // Else add the item to the dictionary
+        else  inventory.Add(_item, _quantity);
+
+        OnItemAdded?.Invoke(_item, inventory[_item]);
+        OnItemQuantityChanged?.Invoke(_item, inventory[_item]);
     }
 
-    public void RemoveItem(ItemNames _item, int _amount)
+    /// <summary>
+    /// Removes a quantity of a specified item from the inventory
+    /// </summary>
+    /// <param name="_item"> The item you wish to remove</param>
+    /// <param name="_quantity"> The quantity you wish to remove</param>
+    public void RemoveItem(ItemNames _item, int _quantity)
     {
-        if (InventoryContainsItem(_item))
+        if (_quantity == 0) Debug.LogWarning("A quantity of 0 was removed");
+
+        // Take the abosulte so either a positive or negative will remove the correct amount
+        _quantity = Mathf.Abs(_quantity);
+
+        if (InventoryDoesNotContainItem(_item))
         {
-            if (inventory[_item] - math.abs(_amount) > 0 && _amount != 0) // Checks that there are enough items to remove, and if there was a number to subtract provided
-            {
-                Mathf.Clamp(inventory[_item] -= math.abs(_amount), 0, 100);
-                OnAmountChangeItem?.Invoke(_item, inventory[_item]);
-                OnRemoveItem?.Invoke(_item, inventory[_item]);
-                if (inventory[_item] == 0) inventory.Remove(_item);
-            }
-            else Debug.LogWarning("Attempted to remove item, but no amount to remove was provided or not enough items remaining. Amount:" + _amount + ". Item: " + _item + ": " + inventory[_item]);
+            Debug.LogWarning("Tried to remove " + _item + " from inventory, but item isn't in inventory. Make sure to check first");
+            return;
         }
-        else Debug.LogWarning("Tried to remove item from inventory, but item isn't in inventory. Make sure to run CheckItem first");
+
+        // If the invenotry does not contain enough quantity of the item to remove the amount
+        if (inventory[_item] - _quantity < 0)
+        {
+            Debug.LogWarning("Insufficient item quantity. Item: " + _item + "\nItem quantity: " + inventory[_item] + "\nQuantity attempted to remove: " + _quantity);
+            return;
+        }
+
+        inventory[_item] -= _quantity;
+
+        OnItemRemoved?.Invoke(_item, inventory[_item]);
+        OnItemQuantityChanged?.Invoke(_item, inventory[_item]);
+
+        if (inventory[_item] == 0) inventory.Remove(_item);
     }
 
     #region Utility functions
