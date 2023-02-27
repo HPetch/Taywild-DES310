6ebf81ac5e12b22c953b1e5d7091a418fb859e4a
MAX ANSELL <1903439@uad.ac.uk>
2023-02-27 14:14:00 +0000

    Revert "Inventory controller complete"
    This reverts commit cd6648d0c179c61bf07361ad96ff73c01fdffb4b.

--- Assets/Scripts/Inventory/InventoryController.cs
+++ Assets/Scripts/Inventory/InventoryController.cs
@@ -9,9 +9,9 @@ public class InventoryController : MonoBehaviour
     public static InventoryController Instance { get; private set; }
 
     #region Events
-    public event Action<ItemNames, int> OnAddItem; // Called when an item is added, returns the new total
-    public event Action<ItemNames, int> OnRemoveItem; // Called when an item is attempted be be subtracted, returns new value and weither the subtraction worked or not
-    public event Action<ItemNames, int> OnAmountChangeItem; // Called when item amount changes, ignores if item was added or subtracted, returns the new total
+    public event Action<int> OnAddItem; // Called when an item is added, returns the new total
+    public event Action<int> OnRemoveItem; // Called when an item is attempted be be subtracted, returns new value and weither the subtraction worked or not
+    public event Action<int> OnAmountChangeItem; // Called when item amount changes, ignores if item was added or subtracted, returns the new total
     #endregion
 
     #region Public variables
@@ -19,9 +19,8 @@ public class InventoryController : MonoBehaviour
     {
         FLOWER, TWIG, ROOT
     }
-
-    private Dictionary<ItemNames, int> inventory = new Dictionary<ItemNames, int>();
     
+    public Dictionary<ItemNames, int> ItemDictionary { get; private set; }
     #endregion
 
     #region Functions
@@ -33,50 +32,39 @@ public class InventoryController : MonoBehaviour
     }
     #endregion
 
-    public int CheckItem(ItemNames _item)
-    {
-        if (InventoryDoesNotContainItem(_item)) return 0;
-        else return inventory[_item];
-    }
-
     public void AddItem(ItemNames _item, int _amount)
     {
+
         if (_amount > 0)
         {
-            if (InventoryDoesNotContainItem(_item))
-            {
-                inventory.Add(_item, _amount);
-            }
-            else
-            {
-                Mathf.Clamp(inventory[_item] += _amount, 0, 100);
-            }
-            OnAmountChangeItem?.Invoke(_item, inventory[_item]);
-            OnAddItem?.Invoke(_item, inventory[_item]);
+            Mathf.Clamp(ItemDictionary[_item] += _amount, 0, 100);
+            OnAmountChangeItem.Invoke(ItemDictionary[_item]);
+            OnAddItem.Invoke(ItemDictionary[_item]);
         }
         else Debug.LogWarning("Attempted to add item, but amount wasn't provided or was negative. Amount: " + _amount);
-        if (inventory[_item] >= 99) Debug.LogWarning("Maximum item value reached. Somthing must have gone wrong. Item: " + _item);
+        
     }
 
     public void RemoveItem(ItemNames _item, int _amount)
     {
-        if (InventoryContainsItem(_item))
+        if (_amount != 0)
         {
-            if (inventory[_item] - math.abs(_amount) > 0 && _amount != 0) // Checks that there are enough items to remove, and if there was a number to subtract provided
+            bool isEnough = (ItemDictionary[_item] + math.abs(_amount)) > 0;
+            if (isEnough)
             {
-                Mathf.Clamp(inventory[_item] -= math.abs(_amount), 0, 100);
-                OnAmountChangeItem?.Invoke(_item, inventory[_item]);
-                OnRemoveItem?.Invoke(_item, inventory[_item]);
-                if (inventory[_item] == 0) inventory.Remove(_item);
+                Mathf.Clamp(ItemDictionary[_item] -= math.abs(_amount), 0, 100);
+                OnAmountChangeItem.Invoke(ItemDictionary[_item]);
+                if (ItemDictionary[_item] == 0)
+                {
+                    ItemDictionary.Remove(_item);
+                }
             }
-            else Debug.LogWarning("Attempted to remove item, but no amount to remove was provided or not enough items remaining. Amount:" + _amount + ". Item: " + _item + ": " + inventory[_item]);
+            OnRemoveItem.Invoke(ItemDictionary[_item]);
         }
-        else Debug.LogWarning("Tried to remove item from inventory, but item isn't in inventory. Make sure to run CheckItem first");
+        else Debug.LogWarning("Attempted to remove item, but no amount to remove was provided. Amount:" + _amount);
     }
-    
-    #region Utility functions
-    private bool InventoryContainsItem(ItemNames _item) { return inventory.ContainsKey(_item); }
-    private bool InventoryDoesNotContainItem(ItemNames _item) { return !inventory.ContainsKey(_item); }
     #endregion
-    #endregion
+
+
+
 }
