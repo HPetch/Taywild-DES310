96e588e34ba1b167cbe29636765573334f385610
Maximilian Ansell <38575320+Wolf1938@users.noreply.github.com>
2023-03-07 10:25:16 +0000

    Added air current fighting (#47)
    Co-authored-by: MAX ANSELL <1903439@uad.ac.uk>

--- Assets/Scripts/Environemt/AirCurrent.cs
+++ Assets/Scripts/Environemt/AirCurrent.cs
@@ -8,12 +8,14 @@ public class AirCurrent : MonoBehaviour
     public enum AirCurrentDirectionTypes { UP, LEFT, RIGHT }
     [SerializeField] public AirCurrentDirectionTypes AirCurrentDirectionType = AirCurrentDirectionTypes.UP;
 
-    public Rect AirCurrentRect { get; private set; }
-
     [field: Range(0, 15)]
     [field: SerializeField] public float AirCurrentSpeed { get; private set; } = 10f;
 
+    [field: Range(0, 15)]
+    [field: SerializeField] public float AirCurrentFightSpeed { get; private set; } = 3f;
+
     [SerializeField] private bool displayDebugInfo = true;
+    private Rect airCurrentRect;
 
     private void Awake()
     {
@@ -22,41 +24,55 @@ public class AirCurrent : MonoBehaviour
         transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = false;
 
         Vector2 size = GetComponent<BoxCollider2D>().size;
-        AirCurrentRect = new Rect((Vector2)transform.position - (size / 2), size);
+        airCurrentRect = new Rect((Vector2)transform.position - (size / 2), size);
     }
 
-#if UNITY_EDITOR
     private void Update()
     {
-        transform.rotation = Quaternion.identity;
-        transform.localScale = Vector3.one;
-
-        ParticleSystem particleSystem = GetComponentInChildren<ParticleSystem>();
-        particleSystem.transform.localPosition = new Vector2(0, -transform.localScale.y / 2);
-
-        if (particleSystem.main.duration != transform.localScale.y)
+#if UNITY_EDITOR
+        if (!Application.isPlaying)
         {
-            particleSystem.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
-            ParticleSystem.MainModule mainModule = particleSystem.main;
-            mainModule.duration = transform.localScale.y;
-            mainModule.startLifetime = transform.localScale.y;
-            particleSystem.Play();
+            transform.rotation = Quaternion.identity;
+            transform.localScale = Vector3.one;
+
+            ParticleSystem particleSystem = GetComponentInChildren<ParticleSystem>();
+            particleSystem.transform.localPosition = new Vector2(0, -transform.localScale.y / 2);
+
+            if (particleSystem.main.duration != transform.localScale.y)
+            {
+                particleSystem.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+                ParticleSystem.MainModule mainModule = particleSystem.main;
+                mainModule.duration = transform.localScale.y;
+                mainModule.startLifetime = transform.localScale.y;
+                particleSystem.Play();
+            }
+
+            ParticleSystem.ShapeModule shape = particleSystem.shape;
+            shape.radius = transform.localScale.x;
+
+
+            if (displayDebugInfo)
+            {
+                transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = true;
+                transform.GetChild(0).transform.localScale = GetComponent<BoxCollider2D>().size;
+                transform.GetChild(0).transform.SetPositionAndRotation(transform.position, transform.rotation);
+            }
+            else
+            {
+                transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = false;
+            }
+
+            return;
         }
+#endif    
 
-        ParticleSystem.ShapeModule shape = particleSystem.shape;
-        shape.radius = transform.localScale.x;
-
-
-        if (displayDebugInfo)
+        if (airCurrentRect.Contains(PlayerController.Instance.transform.position))
         {
-            transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = true;
-            transform.GetChild(0).transform.localScale = GetComponent<BoxCollider2D>().size;
-            transform.GetChild(0).transform.SetPositionAndRotation(transform.position, transform.rotation);
+            PlayerController.Instance.AddAirCurrent(this);
         }
         else
         {
-            transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = false;
+            PlayerController.Instance.RemoveAirCurrent(this);
         }
     }
-#endif    
 }
