492cce192900a2548c0384007a0e2c15d90f6deb
HPetch <60263607+HPetch@users.noreply.github.com>
2023-03-13 15:01:48 +0000

    Added debug UI for testers
    Shows player states, co-ordinates within partition, and partition name. Will be very helpful for bug reporting and equip the testers with what they may need.

---
+++ Assets/Scripts/Utility/Testers/TestersInterface.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TestersInterface : MonoBehaviour
+{
+    #region Variables
+    private Partition currentPartition;
+    private Vector2 positionInPartition;
+    [SerializeField] private TMP_Text partitionText;
+    [SerializeField] private TMP_Text coordsText;
+    [SerializeField] private TMP_Text playerControllerText;
+    #endregion
+
+    private void Update()
+    {
+        //Pass current partition's name into text
+        currentPartition = TransitionController.Instance.CurrentPartition;
+        partitionText.text = "Zone: " + currentPartition.gameObject.name.ToString();
+
+        //Get co-ordinates relative to partition center and display
+        positionInPartition = PlayerController.Instance.transform.position - currentPartition.transform.position; //Get difference between center and player pos
+        coordsText.text = "Pos: " + positionInPartition.ToString("F2");
+
+        //Show detailed state info
+        if (PlayerController.Instance.IsLockedInput) playerControllerText.text = "Input is Locked.";
+        else
+            playerControllerText.text = "Facing right: " + PlayerController.Instance.IsFacingRight + "\n"
+                + "Grounded: " + PlayerController.Instance.IsGrounded + "\n"
+                + "Dashing: " + PlayerController.Instance.IsDashing + "\n"
+                + "Gliding: " + PlayerController.Instance.IsGliding + "\n"
+                + "Touching wall: " + PlayerController.Instance.IsTouchingWall + "\n"
+                + "Sticky wall: " + PlayerController.Instance.IsWallStuck + "\n"
+                + "Wall sliding: " + PlayerController.Instance.IsWallSliding + "\n";
+
+    }
+
+}
