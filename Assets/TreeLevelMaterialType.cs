de9605277ec5f1674ff4c0d76330c5e5ce19d840
HPetch <60263607+HPetch@users.noreply.github.com>
2023-03-21 19:29:22 +0000

    Tree level controller basic shell
    I haven't set up TreeLevelInterface.cs as an Instance, sorry.
    
    You call the Open and Close functions in TreeLevelInterface to trigger the open/close animation.
    
    You can update text fields with UpdateMaterialText, by passing in the material type you want to update, and the integer value to update it to.

---
+++ Assets/TreeLevelMaterialType.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeLevelMaterialType : MonoBehaviour
+{
+    public InventoryController.ItemNames MaterialType; //I know I shouldn't use public but I still struggle with getters/setters and I'm tired... -Harry
+}
