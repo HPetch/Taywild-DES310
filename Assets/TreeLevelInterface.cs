0e6a8f664536cbeb07a09e5c37670895a1c3ca54
HPetch <60263607+HPetch@users.noreply.github.com>
2023-03-21 19:31:01 +0000

    Tree level UI basic (#76)
    * Fix: Images in crafting menu enforce aspect ratio
    
    "Preserve Aspect" is an option that can be enabled for Images that will... preserve the sprite's aspect ratio.
    
    This option is hidden in Debug unless a sprite is given to the Image. I turned on those options so we wouldn't have to do it through code for no reason.
    Thanks Unity.
    
    * Tree level controller basic shell
    
    I haven't set up TreeLevelInterface.cs as an Instance, sorry.
    
    You call the Open and Close functions in TreeLevelInterface to trigger the open/close animation.
    
    You can update text fields with UpdateMaterialText, by passing in the material type you want to update, and the integer value to update it to.

---
+++ Assets/TreeLevelInterface.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TreeLevelInterface : MonoBehaviour
+{
+    #region Object references + variables
+    [SerializeField] private GameObject[] SpriteAtLevel; //What sprite should be shown at each level (0 should be invisible, 1,2,3 relevant cherry blossom etc)
+    [SerializeField] private TMP_Text[] MaterialTexts; //Text components that update
+
+    [Header("Open/close animation")]
+    [SerializeField] private RectTransform interfaceParent; //What element needs moving
+    [SerializeField] private float positionOpened = 16f; //X axis in Rect Transform to go to when opening.
+    private float positionClosed; //X axis position to go to when closing
+    [SerializeField] private float animationDuration = 0.5f;
+    [SerializeField] private LeanTweenType OpenAnimation;
+    [SerializeField] private LeanTweenType CloseAnimation;
+    #endregion
+
+    #region Methods
+
+    private void Start()
+    {
+        positionClosed = interfaceParent.anchoredPosition.x; //Get closed position based on where we put it at the start.
+    }
+
+    //Update relevant text with relevant number
+    private void UpdateMaterialText(InventoryController.ItemNames type, int value)
+    {
+        //Find any texts that have the item type, and update their text component if so
+        foreach (TMP_Text item in MaterialTexts)
+        {
+            if (item.GetComponent<TreeLevelMaterialType>().MaterialType == type)
+            {
+                //Update text with value here
+                item.text = value.ToString();
+            }
+        }
+    }
+    
+    //Slide the interface onto the screen
+    //LTRDescr returns a leantween reference, just in case it is needed
+    private void Open()
+    {
+        LeanTween.cancel(interfaceParent); //Cancel anything that may be happening
+        LeanTween.moveX(interfaceParent, positionOpened, animationDuration).setEase(OpenAnimation);
+
+    }
+
+    //Slide the interface off of the screen
+    private void Close()
+    {
+        LeanTween.cancel(interfaceParent); //Cancel anything that may be happening
+        LeanTween.moveX(interfaceParent, positionClosed, animationDuration).setEase(CloseAnimation);
+    }
+
+    #endregion
+}
