f1eae8a9436a4262eaa09fbb1f3dacf7323b1940
Wolf1938 <38575320+Wolf1938@users.noreply.github.com>
2023-04-25 00:53:31 +0100

    Main Menu Journal

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,7 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using Journal;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
@@ -43,7 +42,7 @@ public class MainMenuController : MonoBehaviour
     #region UI Buttons
     public void PlayButton()
     {
-
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void LoadButton()
