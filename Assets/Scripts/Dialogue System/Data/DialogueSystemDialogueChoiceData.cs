72449f31329d7d579a5be47fbb3ea68d70010c18
TallyTS <124575498+TallyTS@users.noreply.github.com>
2023-05-08 19:01:27 +0100

    Lucus Dialogue Progression Fixed
    Was an implementation issue all along

--- Assets/Scripts/Dialogue System/Data/DialogueSystemDialogueChoiceData.cs
+++ Assets/Scripts/Dialogue System/Data/DialogueSystemDialogueChoiceData.cs
@@ -5,7 +5,7 @@ namespace DialogueSystem.Types
 {
     public enum NodeTypes { Dialogue, Edge, Delay, Audio, GetQuest, SetQuest, Graph }
     public enum DialogueTypes { SingleChoice, MultipleChoice }
-    public enum QuestTypes { WarsanTutorial, WarsanBenchQ1, WarsanLogPlanterQ2, LucasFlowerQ1a, LucusFlowerQ1b, LucusBerriesQ2, LuWaGreenhouseQ1a, LuWaGreenhouseQ1b, LuWaBakedQ2a, LuWaBakedQ2b, LuWaIngredientsQ3a, LuWaIngredientsQ3b, LucusIntro, }
+    public enum QuestTypes { WarsanTutorial, WarsanBenchQ1, WarsanLogPlanterQ2, LucasFlowerQ1a, LucusFlowerQ1b, LucusFlowerQ1bFix, LucusBerriesQ2, LuWaGreenhouseQ1a, LuWaGreenhouseQ1b, LuWaBakedQ2a, LuWaBakedQ2b, LuWaIngredientsQ3a, LuWaIngredientsQ3b, LucusIntro, }
     public enum QuestStates { NotAccepted, InProgress, HandIn, Completed }
 }
 
